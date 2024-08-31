2c900932639963954171faa581adabcc4d9d6cb2
MoSizzle87 <105734122+MoSizzle87@users.noreply.github.com>
2024-08-31 15:55:36 +0200

    Update CronJob.sh

--- ETL/Extract/CronJob.sh
+++ ETL/Extract/CronJob.sh
@@ -1,2 +1,2 @@
 # Execute le sraping des offres du site welcome to the jumgle tous les jours à 3 heures du matin
-0 3 * * * /home/ubuntu/ETL/Extract/run_docker_compose.sh >> /home/ubuntu/ETL/Extract/docker_compose_cron.log 2>&1
+0 3 * * * /home/ubuntu/ETL/run_scraping.sh >> /home/ubuntu/ETL/run_scraping.log 2>&1
